4b33cd44cde25fa0d1313be99cfbd0db06be8c31
ZeroSystems <sales@erpzero.com>
2019-12-21 04:00:23 +0200

    Add files via upload

---
+++ zero_voip/models/res_config_settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        params = self.env['ir.config_parameter'].sudo()
+        res.update(
+            wsServer=params.get_param('voip.wsServer', default='wss://edge.sip.onsip.com'),
+        )
+        return res
